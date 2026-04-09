9787388cc9e5e398ab15c223380a9c0380450e9b
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2026-04-09 10:47:13 -0300

    Update EmailService.php

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -20,11 +20,22 @@ class EmailService {
             'mail.mailers.smtp.host' => $emailSettings->mail_host ?? 'smtp.gmail.com',
             'mail.mailers.smtp.port' => $emailSettings->mail_port ?? 465,
             'mail.mailers.smtp.encryption' => $emailSettings->mail_encryption ?? 'ssl',
-            'mail.mailers.smtp.username' => $emailSettings->mail_username ?? 'waggner.447@gmail.com',
-            'mail.mailers.smtp.password' => $emailSettings->mail_password ?? 'aggd cvvg ljkp gxli',
-            'mail.from.address' => $emailSettings->mail_from_address ?? 'waggner.447@gmail.com',
+            'mail.mailers.smtp.username' => $emailSettings->mail_username ?? '',
+            'mail.mailers.smtp.password' => $emailSettings->mail_password ?? '',
+            'mail.from.address' => $emailSettings->mail_from_address ?? '',
             'mail.from.name' => $emailSettings->mail_from_name ?? 'WHI - Web de Alta inspiração',
         ]);      
+        // Config::set([
+        //     'mail.default' => $emailSettings->mail_mailer ?? 'smtp',
+        //     'mail.mailers.smtp.transport' => $emailSettings->mail_mailer ?? 'smtp',
+        //     'mail.mailers.smtp.host' => $emailSettings->mail_host ?? 'smtp.gmail.com',
+        //     'mail.mailers.smtp.port' => $emailSettings->mail_port ?? 465,
+        //     'mail.mailers.smtp.encryption' => $emailSettings->mail_encryption ?? 'ssl',
+        //     'mail.mailers.smtp.username' => $emailSettings->mail_username ?? 'waggner.447@gmail.com',
+        //     'mail.mailers.smtp.password' => $emailSettings->mail_password ?? 'aggd cvvg ljkp gxli',
+        //     'mail.from.address' => $emailSettings->mail_from_address ?? 'waggner.447@gmail.com',
+        //     'mail.from.name' => $emailSettings->mail_from_name ?? 'WHI - Web de Alta inspiração',
+        // ]);      
 
     }    
 }
