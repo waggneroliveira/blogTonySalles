09b99ca63a74e694c1726ad2343cb428d718b1fa
Wagner Oliveira <waggner.dev@gmail.com>
2026-09-01 17:24:59 -0300

    Update form.blade.php

--- resources/views/admin/blades/report/form.blade.php
+++ resources/views/admin/blades/report/form.blade.php
@@ -13,30 +13,32 @@
 </div>
 <div class="row g-3">
     <div class="mb-3 col-12">
-        <label for="description" class="form-label">link</label>
+        <label for="description" class="form-label">Link</label>
         <input 
             type="text" 
-            name="link" 
+            name="description" 
             class="form-control" 
             id="description{{ isset($report->id) ? $report->id : '' }}" 
             value="{{ isset($report) ? $report->description : '' }}" 
-            placeholder="Digite seu nome"
+            placeholder="Link"
         >
     </div>
 </div>
 
 <div class="mb-3 col-12">
-    <label for="path_file" class="form-label">Arquivo</label>
-    <input 
-        type="file" 
-        name="path_file" 
-        accept="application/pdf" 
-        data-plugins="dropify" 
-        data-default-file="{{ isset($report) && $report->path_file != '' ? url('storage/'.$report->path_file) : '' }}" 
-        class="form-control"
-    />
-    <p class="text-muted text-center mt-2 mb-0">
-        {{ __('dashboard.text_img_size') }} <b class="text-danger">3 MB</b>.
-    </p>
+    <div class="form-check">
+        <input 
+            name="active" 
+            {{ isset($report->active) && $report->active == 1 ? 'checked' : '' }} 
+            type="checkbox" 
+            class="form-check-input" 
+            id="invalidCheck{{ isset($report->id) ? $report->id : '' }}" 
+        />
+        <label class="form-check-label" for="invalidCheck{{ isset($report->id) ? $report->id : '' }}">
+            {{ __('dashboard.active') }}?
+        </label>
+        <div class="invalid-feedback">
+            You must agree before submitting.
+        </div>
+    </div>
 </div>
-
